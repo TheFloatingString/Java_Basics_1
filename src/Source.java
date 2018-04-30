1d5e97c5e4778dc6fa64dcfe51e35d38e5811402
Laurence <laurencel2001@gmail.com>
2018-04-30 21:21:25 +0000

    Update Source.java

--- src/Source.java
+++ src/Source.java
@@ -1,5 +1,10 @@
-import java.util.List;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Set;
 
 public class Source {
 
@@ -17,6 +22,48 @@ public class Source {
 		liste.remove(0);
 		
 		System.out.println(liste);
+		
+		LinkedList<String> noms = new LinkedList<>();
+		noms.add("Mathieu");
+		noms.add("Laurent");
+		noms.add("Alex");
+		noms.add("Omar");
+		
+		LinkedList<String> cap_nom = new LinkedList<>();
+		
+		for(String item:noms){
+			cap_nom.add(item.toUpperCase());
+		}
+		
+		Collections.sort(noms);
+		Collections.sort(cap_nom);
+		
+		System.out.println(noms);
+		System.out.println(cap_nom);
+		System.out.println(cap_nom.get(0));
+		
+		Set<String> set = new HashSet<>();
+		
+		set.add("abc");
+		set.add("hef");
+		set.add("ghi");
+		
+		
+		
+		Set<String> newSet = new HashSet<>();
+		
+		
+		for(String item : set){
+			newSet.add(item.toUpperCase());
+		}
+		
+		System.out.println(newSet);
+		
+		HashMap dict = new HashMap();
+		dict.put("Phil", "Programmer");
+		dict.put("Mathieu", "Programmer");
+		System.out.println(dict.get("Phil"));
+		System.out.println(dict.keySet());
 		
 	}
 	
